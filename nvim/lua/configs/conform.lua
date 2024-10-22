d3cc1e4ec3e774207dcb2642d4c3948360ac4f1f
Bogdan Buduroiu <bogdan@buduroiu.com>
2024-10-22 18:40:01 +0800

    use conform.nvim as universal formatting handler

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -3,14 +3,22 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
-    python = { "ruff", "pyright" }
+    python = { "ruff_format" },
+    go = { "goimports", "gofmt" },
+    json = { "jq" },
   },
 
   format_on_save = {
     -- These options will be passed to conform.format()
     timeout_ms = 500,
-    lsp_fallback = true,
+    lsp_format = "fallback",
   },
+  -- Set the log level. Use `:ConformInfo` to see the location of the log file.
+  log_level = vim.log.levels.ERROR,
+  -- Conform will notify you when a formatter errors
+  notify_on_error = true,
+  -- Conform will notify you when no formatters are available for the buffer
+  notify_no_formatters = true,
 }
 
 return options
