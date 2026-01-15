baa40f3a2bc68496e764ce5482b56b4b1f28048b
Bogdan Buduroiu <bogdan@buduroiu.com>
2026-01-15 13:39:55 +0800

    chore: add Yubikey 2 (+update age)

--- home/terminal/default.nix
+++ home/terminal/default.nix
@@ -1,4 +1,4 @@
-{ config, ... }: let
+{ config, pkgs, ... }: let
   data = config.xdg.dataHome;
   conf = config.xdg.configHome;
   cache = config.xdg.cacheHome;
@@ -10,5 +10,9 @@ in {
 
   home.sessionVariables = {
     EDITOR = "nvim";
+    # Use pinentry-qt for age-plugin-yubikey (PIV) PIN prompts
+    PINENTRY_PROGRAM = "${pkgs.pinentry-qt}/bin/pinentry-qt";
+    # Explicit path to age keys for SOPS (needed for YubiKey plugin discovery)
+    SOPS_AGE_KEY_FILE = "${conf}/sops/age/keys.txt";
   };
 }
